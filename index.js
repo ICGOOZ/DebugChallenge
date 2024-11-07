66e8a2840cdb89e2133233fdb8de4ca261d222d2
ICGOOZ <jwangsgaard97@s.tooeletech.edu>
2024-11-07 13:06:23 -0700

    Ready to submit

---
+++ index.js
@@ -0,0 +1,12 @@
+// make sure these match the HTML element names for the ID then class.
+let input = document.querySelector("#name");
+let display = document.querySelector(".displaymycode");
+
+// This code should be working
+//input.addEventListener("input.value", luna);//
+
+function simpleMath(a, b) {
+  return a + b;
+}
+
+console.log(simpleMath(2, 3));
